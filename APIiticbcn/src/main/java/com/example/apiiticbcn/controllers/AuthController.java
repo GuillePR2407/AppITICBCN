10e4069ab85eb99f5ab5fca324eec2d4fdf10829
Francisco Jordan Garcia <franjoga24@gmail.com>
2024-05-02 15:31:30 +0200

    a

--- APIiticbcn/src/main/java/com/example/apiiticbcn/controllers/AuthController.java
+++ APIiticbcn/src/main/java/com/example/apiiticbcn/controllers/AuthController.java
@@ -87,7 +87,7 @@ public class AuthController {
             .body(new UserInfoResponse(userDetails.getId(),
                     userDetails.getUsername(),
                     userDetails.getEmail(),
-                    roles));
+                    roles, jwtToken));
   }
   /*
   {
